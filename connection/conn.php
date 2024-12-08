415e2e261c61dc8871b6d21adc004bfd26218dcd
Turkiismail <turkiismail07@gmail.com>
2024-12-08 18:23:01 +0100

    added connection in a folder

---
+++ connection/conn.php
@@ -0,0 +1,7 @@
+<?php
+    define("server", "localhost");
+    define("user", "root");
+    define("password", "");
+    define("db", "ecoquest");
+    $conn=mysqli_connect(server, user, password, db);
+?>
