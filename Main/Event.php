7e65e78f9c7d4814b1203a506d345c88ad6893a2
imenSammari <imensammari97@gmail.com>
2025-01-14 22:43:15 +0100

    hatit logo

--- Main/Event.php
+++ Main/Event.php
@@ -23,9 +23,8 @@
         class="bg-[#044952] dark:bg-[#044952] fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
-            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Logo">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LOGO</span>
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="logo equoquest imen-06.png" class="h-12" alt="Logo">
             </a>
 
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -230,7 +229,10 @@
         <div
             class="container mx-auto px-4 space-y-4 md:space-y-0 md:flex md:justify-between sm:items-center sm:justify-center">
             <div>
-                <p class="text-lg font-bold">LOGO</p>
+            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="logo equoquest imen-06.png" class="h-12" alt="Logo">
+            </a>
+
                 <p>Contact</p>
                 <p>Email: ecoquest@gmail.com</p>
                 <p>Phone: +216 56 650 772</p>
--- Main/becomePartner.php
+++ Main/becomePartner.php
@@ -119,10 +119,10 @@
      Header -->
     <nav class="bg-[#044952] dark:bg-[#044952] fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Logo">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LOGO</span>
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="logo equoquest imen-06.png" class="h-12" alt="Logo">
             </a>
+
 
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button type="button"
